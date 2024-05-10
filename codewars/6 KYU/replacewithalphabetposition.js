95ff10754f8813306cb143db6b58dcf8424e2b8c
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-05-10 14:51:13 +0200

    update

--- codewars/6 KYU/replacewithalphabetposition.js
+++ codewars/6 KYU/replacewithalphabetposition.js
@@ -4,16 +4,22 @@
 
 // "a" = 1, "b" = 2, etc.
 
-function alphabetPosition(text) {
-    let newOne = text.split('')
-    let result = []
-    for(let i=0;i<newOne.length;i++){
-        let letter = newOne[i].toLocaleLowerCase()
-        if(letter >='a' && letter<='z'){
-        let position= letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1
-        result.push(position)
-    }
-    }
-    return result.join(',')
-  }
-  console.log(alphabetPosition("aBc"));
+// function alphabetPosition(text) {
+//     let newOne = text.split('')
+//     let result = []
+//     for(let i=0;i<newOne.length;i++){
+//         let letter = newOne[i].toLocaleLowerCase()//convert to lower case
+//         if(letter >='a' && letter<='z'){//check if it is a letter
+//         let position= letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1//convert letter to number
+//         result.push(position)
+//     }
+//     }
+//     return result.join(',')
+//   }
+
+function alphabetPosition (text)
+{
+    return [...text].map(el => 'abcdefghijklmnopqrstuvwxyz'.indexOf(el) != -1 ? 'abcdefghijklmnopqrstuvwxyz'.indexOf(el) + 1 : el).join``
+
+}  console.log(alphabetPosition("abc"));
+  console.log(alphabetPosition("n+bk$&;{"));
