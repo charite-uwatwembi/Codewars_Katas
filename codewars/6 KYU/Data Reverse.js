397a9ce7085c56234e37c4515bf18a68f2fb5ac2
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-27 11:42:20 +0200

    data reverse

--- codewars/6 KYU/Data Reverse.js
+++ codewars/6 KYU/Data Reverse.js
@@ -23,6 +23,7 @@
         result.push(data.slice(i, i + 8));
     }
     return result.reverse().flat();
+    
 }
 
 console.log(dataReverse([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]));
