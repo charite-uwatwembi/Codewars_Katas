7eb4cb7f8bd032e2d1f57e2a34fc28262dc444ec
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-07-11 11:30:08 +0200

    dashatize it

---
+++ codewars/6 KYU/dashatize-it.js
@@ -0,0 +1,28 @@
+function dashatize(num) {
+    let arr = num.toString();
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) { // Check if the digit is odd
+            // Ensure not to add leading or trailing dashes incorrectly
+            if (i > 0 && result[result.length - 1] !== '-') {
+                result.push('-');
+            }
+            result.push(arr[i]);
+            if (i < arr.length - 1 && arr[i + 1] % 2 !== 0) {
+                result.push('-');
+            }
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result.join('');
+}
+
+// Test cases
+console.log(dashatize(274));  // Output: '2-7-4'
+console.log(dashatize(6815));  // Output: '68-1-5'
+console.log(dashatize(13579));  // Output: '1-3-5-7-9'
+console.log(dashatize(2468));  // Output: '2468'
+console.log(dashatize(0));  // Output: '0'
